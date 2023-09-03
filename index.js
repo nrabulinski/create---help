1c4401c871b9c911452308c214cf7b228a9b35e9
Nikodem Rabuliński <nikodem@rabulinski.com>
2023-09-03 19:40:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+console.log("You have been pwned");
+console.log("(actually not but you could've been)");
+console.log("npm is broken");
+console.log("You just executed a package called create---help :)");
